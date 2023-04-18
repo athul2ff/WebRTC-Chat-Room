006da430051267102e6db5fed745ff1e3e282477
athul <athul.p@zweck.io>
2023-04-18 23:04:22 +0530

    adding signaling with Agora RTM

--- main.js
+++ main.js
@@ -1,26 +1,87 @@
+let APP_ID = "612fd3f7a78d4656b2b85859c13567df";
+
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000));
+
+let client;
+let channel;
+
 let localStream;
 let remoteStream;
 let peerConnection;
 
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+};
+
 let init = async () => {
+  client = await AgoraRTM.createInstance(APP_ID);
+  await client.login({ uid, token });
+
+  channel = client.createChannel("main");
+  await channel.join();
+
+  channel.on("MemberJoined", handleUserJoined);
+
+  client.on("MessageFromPeer", handleMessageFromPeer);
+
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false,
   });
   document.getElementById("user-1").srcObject = localStream;
-
-  createOffer();
 };
 
-let createOffer = async () => {
-  peerConnection = new RTCPeerConnection();
+let handleMessageFromPeer = async (message, memberId) => {
+  message = JSON.parse(message.text);
+  console.log("Message:", message);
+};
 
+let handleUserJoined = async (memberId) => {
+  console.log("A new user joined the channel:", memberId);
+  createOffer(memberId);
+};
+
+let createOffer = async (memberId) => {
+  peerConnection = new RTCPeerConnection(servers);
   remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack();
+    });
+  };
+
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      client.sendMessageToPeer(
+        {
+          text: JSON.stringify({
+            type: "candidate",
+            candidate: event.candidate,
+          }),
+        },
+        memberId
+      );
+    }
+  };
 
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
 
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "offer", offer: offer }) },
+    memberId
+  );
 };
 
 init();
